2102efd8e94ae5930220b1e9f97068ab50494537
joelthegoat21 <729037@student.dusd.net>
2025-04-08 15:46:10 -0700

    i canm kill animals
    today i made my projectiles kill animals and made the game over screen

--- Unit2BasicGamePlayPineiro8/Assets/Scripts/PlayerController.cs
+++ Unit2BasicGamePlayPineiro8/Assets/Scripts/PlayerController.cs
@@ -13,7 +13,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -26,13 +26,14 @@ public class PlayerController : MonoBehaviour
 
         if (transform.position.x > xRange)
         {
+
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
-        if (Input.GetKeyDown(KeyCode.Space))        
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
